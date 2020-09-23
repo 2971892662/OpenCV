965f614977c33fa2d4ea2199e066c76af5cdd393
李泽鹏 <2971892662@qq.com>
2020-09-23 20:23:25 +0800

    chapter10.1

---
+++ 学习《Opencv3》/练习题代码/第10章/第1题/源码.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+    Mat src = imread("F:/1314.jpg");
+    Mat mat1, mat2, mat3, mat4, mat5;
+    GaussianBlur(src, mat1, Size(3 , 3), 0, 0);
+    GaussianBlur(src, mat2, Size(5,5), 0, 0);
+    GaussianBlur(src, mat3, Size(9,9), 0, 0);
+    GaussianBlur(src, mat4, Size(11,11), 0, 0);
+    GaussianBlur(mat2, mat5, Size(3, 3), 0, 0);
+    imshow("原图",src);
+    imshow("3*3", mat1);
+    imshow("5*5", mat2);
+    imshow("9*9", mat3);
+    imshow("11*11", mat4);
+    imshow("两次5*5", mat5);
+    waitKey(0);
+    return 0;
+}
+
+
---
+++ 学习《Opencv3》/练习题代码/第8章/第二题/2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+class Para{
+public:
+    Mat dest;
+};
+
+void TestCallBack(int event, int x, int y, int flags, void* param);
+
+int main()
+{
+    Mat img = imread("./videos/1.jpg");
+    Para param;
+    img.copyTo(param.dest);
+    namedWindow("show", WINDOW_AUTOSIZE);
+    imshow("show", img);
+    setMouseCallback("show", TestCallBack, &param);
+    waitKey(0);
+    destroyWindow("show");
+    return 0;
+}
+
+void TestCallBack(int event,int x,int y,int flags, void* param) {
+    Para* p = (Para*)param;
+    String s;
+
+    if (event == EVENT_LBUTTONDOWN) {
+        s.append("P:("); s.append(to_string(x)); s.append(","); s.append(to_string(y)); s.append(")"); s.append(" C:");
+        s.append("("); s.append(to_string(p->dest.at<Vec3b>(y, x)[0])); s.append(","); s.append(to_string(p->dest.at<Vec3b>(y, x)[1])); s.append(",");
+        s.append(to_string(p->dest.at<Vec3b>(y, x)[2])); s.append(")");
+       putText(p->dest,s , Point(x,y), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(0, 0, 255), 2, 8, false);
+       imshow("show", p->dest);
+    }
+}
